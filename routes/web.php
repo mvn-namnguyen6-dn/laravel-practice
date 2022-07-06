55d194c4de85b02e5e6d53983d0495b94944ae94
mvn-namnguyen6-dn <nam.nguyen6@monstar-lab.com>
2022-07-06 09:24:19 +0700

    main

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello',function(){
+    return view('index');
+});
+
+Route::get('/listuser','UserController@index');
+// Route::get('/listuser/{id}','UserController@show');
+Route::get('/users/{user}','UserController@show');
+Route::get('/redirect','UserController@redirectRoute');
+
